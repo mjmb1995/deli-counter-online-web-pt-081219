c74a922263dc185e12ec24dd4e7434e38c7bc90d
Mark Bihag <markbihag330@gmail.com>
2019-09-05 05:19:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,12 @@ katz_deli = []
 def line(array)
   if array.size == 0
     puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    counter = 1
+    array.each do |name|
+      string += "#{counter}. #{name}"
+      counter += 1
+    end
   end
 end
